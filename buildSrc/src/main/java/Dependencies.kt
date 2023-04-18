0d6d52b095a6be1ffbc4d2f21e85300c8dacfc11
benjahjp <benjahjp@gmail.com>
2023-04-18 10:52:43 +0300

    feat: dependencies

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,5 +1,15 @@
 object Version{
     const val core = "1.8.0"
+
+    //hilt
+    const val hilt = "2.44.2"
+    const val hiltCompiler = "1.0.0"
+    const val hiltComposeNavigation = "1.0.0"
+
+    //retrofit
+    const val retrofit = "2.9.0"
+    const val okhttp = "5.0.0-alpha.2"
+    const val logging = "4.10.0"
 }
 
 object Deps{
@@ -9,7 +19,18 @@ object Deps{
     const val constraintLayout = ""
     const val testRunner = ""
 }
-
+object DaggerHilt {
+    const val hilt = "com.google.dagger:hilt-android:${Version.hilt}"
+    const val hiltAndroidCompiler = "com.google.dagger:hilt-compiler:${Version.hilt}"
+    const val hiltCompiler = "androidx.hilt:hilt-compiler:${Version.hiltCompiler}"
+    const val hiltComposeNavigation = "androidx.hilt:hilt-navigation-compose:${Version.hiltComposeNavigation}"
+}
+object Retrofit {
+    const val retrofit = "com.squareup.retrofit2:retrofit:${Version.retrofit}"
+    const val gsonConverter = "com.squareup.retrofit2:converter-gson:${Version.retrofit}"
+    const val okhttp = "com.squareup.okhttp3:okhttp:${Version.okhttp}"
+    const val logging = "com.squareup.okhttp3:logging-interceptor:${Version.logging}"
+}
 object TestImplementation {
     const val kotlinxCoroutine = ""
     const val mockito = ""
