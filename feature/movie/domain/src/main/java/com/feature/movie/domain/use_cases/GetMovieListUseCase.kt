e84900b438b4483e59a5958985f13a071f1c23b0
benjahjp <benjahjp@gmail.com>
2023-04-18 12:00:52 +0300

    feat: movie use case

--- feature/movie/domain/src/main/java/com/feature/movie/domain/use_cases/GetMovieListUseCase.kt
+++ feature/movie/domain/src/main/java/com/feature/movie/domain/use_cases/GetMovieListUseCase.kt
@@ -2,8 +2,25 @@ package com.feature.movie.domain.use_cases
 
 import com.core.common.Resource
 import com.feature.movie.domain.model.Movie
+import com.feature.movie.domain.repository.MovieRepository
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
 
-class GetMovieListUseCase {
-    operator fun invoke(apiKey: String, query: String) = flow<Resource<List<Movie>>> {  }
+class GetMovieListUseCase @Inject constructor(
+    private val movieRepository: MovieRepository
+){
+    operator fun invoke(apiKey: String, query: String): Flow<Resource<List<Movie>>> = flow {
+        try {
+            emit(Resource.Loading())
+            val movies = movieRepository.getMovieList(apiKey, query)
+            emit(Resource.Success(movies))
+        } catch (e: HttpException){
+            emit(Resource.Error(e.localizedMessage ?: "Unknown error occurred"))
+        } catch (e: IOException){
+            emit(Resource.Error("Check internet connection"))
+        }
+    }
 }
