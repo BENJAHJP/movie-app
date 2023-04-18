44fbf76b22a12f5bce835b74d1cb180cb91cb010
benjahjp <benjahjp@gmail.com>
2023-04-18 11:12:37 +0300

    feat: movie api

--- core/network/src/main/java/com/core/network/MovieApi.kt
+++ core/network/src/main/java/com/core/network/MovieApi.kt
@@ -1,8 +1,13 @@
 package com.core.network
 
+import com.core.network.dto.MovieListResponse
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface MovieApi {
-    @GET
-    suspend fun getMovieList()
+    @GET("3/search/movie")
+    suspend fun getMovieList(
+        @Query("api_key") apiKey: String,
+        @Query("query") query: String
+    ): MovieListResponse
 }
--- core/network/src/main/java/com/core/network/dto/MovieListResponse.kt
+++ core/network/src/main/java/com/core/network/dto/MovieListResponse.kt
@@ -1,3 +1,8 @@
 package com.core.network.dto
 
-data class MovieListResponse()
+data class MovieListResponse(
+    val page: Int,
+    val results: List<MovieDto>,
+    val total_pages: Int,
+    val total_results: Int
+)
