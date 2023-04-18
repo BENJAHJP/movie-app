2b02a1587871bdef9fd9606891ec8fae0662e798
benjahjp <benjahjp@gmail.com>
2023-04-18 11:16:28 +0300

    feat: di

--- core/network/src/main/java/com/core/network/di/NetworkModule.kt
+++ core/network/src/main/java/com/core/network/di/NetworkModule.kt
@@ -1,4 +1,23 @@
 package com.core.network.di
 
+import com.core.network.MovieApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+@Module
+@InstallIn(SingletonComponent::class)
 object NetworkModule {
+
+    @Provides
+    fun provideMovieApi(): MovieApi{
+        return Retrofit.Builder()
+            .baseUrl("https://api.themoviedb.org/3/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(MovieApi::class.java)
+    }
 }
