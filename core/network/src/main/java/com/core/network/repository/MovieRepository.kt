7c2a3ec6e02d7e69d6d2a506c517976ed6856ce1
benjahjp <benjahjp@gmail.com>
2023-04-18 11:26:50 +0300

    fix: data providers

--- core/network/src/main/java/com/core/network/repository/MovieRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.core.network.repository
-
-import com.core.network.dto.MovieListResponse
-
-interface MovieRepository {
-    suspend fun getMovieList(apiKey: String, query: String): MovieListResponse
-}
