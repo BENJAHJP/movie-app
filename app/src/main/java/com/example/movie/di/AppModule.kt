1fcae7c04e7eb7e5e72fc93bbfb2cecc5df3b6ea
benjahjp <benjahjp@gmail.com>
2023-04-18 12:41:54 +0300

    feat: navigation

--- app/src/main/java/com/example/movie/di/AppModule.kt
+++ app/src/main/java/com/example/movie/di/AppModule.kt
@@ -1,4 +1,18 @@
 package com.example.movie.di
 
+import com.example.movie.navigation.NavigationProvider
+import com.feature.movie.ui.navigation.MovieNavigationApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Provides
+    fun provideNavigationProvider(movieNavigationApi: MovieNavigationApi): NavigationProvider{
+        return NavigationProvider(movieNavigationApi)
+    }
 }
