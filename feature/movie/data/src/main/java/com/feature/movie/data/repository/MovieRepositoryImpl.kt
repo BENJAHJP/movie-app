6c48b5ff58af5b772391fc69fbfc3ab932178d09
benjahjp <benjahjp@gmail.com>
2023-04-18 11:38:36 +0300

    feat: movie domain layer

---
+++ feature/movie/data/src/main/java/com/feature/movie/data/repository/MovieRepositoryImpl.kt
@@ -0,0 +1,13 @@
+package com.feature.movie.data.repository
+
+import com.core.network.data_providers.MovieDataProviders
+import com.feature.movie.domain.model.Movie
+import com.feature.movie.domain.repository.MovieRepository
+
+class MovieRepositoryImpl @Inject constructor(
+    private val movieDataProviders: MovieDataProviders
+): MovieRepository{
+    override suspend fun getMovieList(apiKey: String, query: String): List<Movie> {
+        return movieDataProviders.getMovieList(apiKey, query)
+    }
+}
--- feature/movie/domain/src/main/java/com/feature/movie/domain/repository/MovieRepository.kt
+++ feature/movie/domain/src/main/java/com/feature/movie/domain/repository/MovieRepository.kt
@@ -1,5 +1,7 @@
 package com.feature.movie.domain.repository
 
+import com.feature.movie.domain.model.Movie
+
 interface MovieRepository {
-    suspend fun getMovieList(apiKey: String, query: String):
+    suspend fun getMovieList(apiKey: String, query: String): List<Movie>
 }
