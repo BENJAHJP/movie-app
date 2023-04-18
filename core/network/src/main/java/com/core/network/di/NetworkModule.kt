d55c8a3726d3a4cadf277878085e9ce9104c886b
benjahjp <benjahjp@gmail.com>
2023-04-18 11:22:45 +0300

    fix: di

--- core/network/src/main/java/com/core/network/di/NetworkModule.kt
+++ core/network/src/main/java/com/core/network/di/NetworkModule.kt
@@ -7,12 +7,14 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
 
     @Provides
+    @Singleton
     fun provideMovieApi(): MovieApi{
         return Retrofit.Builder()
             .baseUrl("https://api.themoviedb.org/3/")
