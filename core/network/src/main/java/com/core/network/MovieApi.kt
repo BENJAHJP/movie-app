3aafe67d5c50e508babece3a2455abe8785fef1a
benjahjp <benjahjp@gmail.com>
2023-04-18 11:03:52 +0300

    feat: dto

--- core/network/src/main/java/com/core/network/MovieApi.kt
+++ core/network/src/main/java/com/core/network/MovieApi.kt
@@ -1,4 +1,8 @@
 package com.core.network
 
+import retrofit2.http.GET
+
 interface MovieApi {
+    @GET
+    suspend fun getMovieList()
 }
---
+++ core/network/src/main/java/com/core/network/dto/MovieListResponse.kt
@@ -0,0 +1,3 @@
+package com.core.network.dto
+
+data class MovieListResponse()
