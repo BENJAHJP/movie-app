74817819fc8484dcdcd5724a45310538d53a722c
benjahjp <benjahjp@gmail.com>
2023-04-19 12:24:12 +0300

    feat: ui

--- feature/movie/ui/src/main/java/com/feature/movie/ui/navigation/InternalMovieFeatureNavigationApi.kt
+++ feature/movie/ui/src/main/java/com/feature/movie/ui/navigation/InternalMovieFeatureNavigationApi.kt
@@ -1,9 +1,12 @@
 package com.feature.movie.ui.navigation
 
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
 import com.core.common.navigation_constants.MovieFeature
 import com.core.feature_api.FeatureApi
+import com.feature.movie.ui.screen.MovieScreen
+import com.feature.movie.ui.screen.MovieScreenViewModel
 
 internal object InternalMovieFeatureNavigationApi: FeatureApi {
     override fun registerGraph(
@@ -14,7 +17,10 @@ internal object InternalMovieFeatureNavigationApi: FeatureApi {
             startDestination = MovieFeature.MovieScreenRoute,
             route = MovieFeature.nestedRoute,
         ){
-
+            composable(MovieFeature.MovieScreenRoute){
+                val viewModel = hiltViewModel<MovieScreenViewModel>()
+                MovieScreen(viewModel)
+            }
         }
     }
 }
