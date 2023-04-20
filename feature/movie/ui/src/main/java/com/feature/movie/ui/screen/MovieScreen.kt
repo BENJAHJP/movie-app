faf8775328e7199ebb998e5002da3af83f2d292d
benjahjp <benjahjp@gmail.com>
2023-04-20 11:30:03 +0300

    fix: moviescreen

--- feature/movie/ui/src/main/java/com/feature/movie/ui/screen/MovieScreen.kt
+++ feature/movie/ui/src/main/java/com/feature/movie/ui/screen/MovieScreen.kt
@@ -1,16 +1,13 @@
 package com.feature.movie.ui.screen
 
-import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Search
 import androidx.compose.material3.CircularProgressIndicator
@@ -21,13 +18,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
 
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MovieScreen(
@@ -48,7 +42,10 @@ fun MovieScreen(
             )
         }
     ){
-        Box {
+        Box(
+            modifier = Modifier.fillMaxSize()
+                .padding(top = it.calculateTopPadding())
+        ){
             if (state.isLoading){
                 CircularProgressIndicator(
                     modifier = Modifier.align(
